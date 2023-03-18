3cc213495a5fe396759b8bcc1c357e6ecef9c3c3
marybloodyzz <112670928+marybloodyzz@users.noreply.github.com>
2023-03-18 15:02:49 -0400

    Initial Commit

---
+++ WristWash Watch App/WristWashApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristWashApp.swift
+//  WristWash Watch App
+//
+//  Created by 张力哲 on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WristWash_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
